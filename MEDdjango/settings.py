9aa0d472e8e6a50d94fdbc144af98698009e8a09
Pat <patrick.fleckenstein@outlook.com>
2022-06-13 04:07:45 -0700

    added a comma

--- MEDdjango/settings.py
+++ MEDdjango/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Created Apps
-    'PressureVesselStressApp'
+    'PressureVesselStressApp',
 ]
 
 MIDDLEWARE = [
