04e555230285898c813c7f9a22e0677224c2ef3f
Pat <patrick.fleckenstein@outlook.com>
2022-06-13 04:06:30 -0700

    updated django model

--- PressureVesselStressApp/models.py
+++ PressureVesselStressApp/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 
 # Create your models here.
-class PressureVesselStressApp(models.Model)
-    SURFACES = ('Inner', 'Outer')
-    CAPPED = ('Capped', 'Uncapped')
 
-    surface = models.CharField(max_length=1, choices=SURFACES)
-    capped = models.CharField(max_length=1, choices=CAPPED)
+
+class PressureVesselStressApp(models.Model):
+
+    INNER = 'IN'
+    OUTER = 'OUT'
+    SURFACES = [
+        (INNER, 'Inner'),
+        (OUTER, 'Outer')
+    ]
+
+    CAPPED = 'C'
+    UNCAPPED = 'U'
+    CAP = [
+        (CAPPED, 'Capped'),
+        (UNCAPPED, 'Uncapped')
+    ]
+
+    surface = models.CharField(max_length=100, choices=SURFACES)
+    capped = models.CharField(max_length=100, choices=CAP)
     pouter = models.FloatField(max_length=100)
     pinner = models.FloatField(max_length=100)
     douter = models.FloatField(max_length=100)
