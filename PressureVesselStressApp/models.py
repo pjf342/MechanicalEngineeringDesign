0609ee1f47f8c3c165036745ae3ae7806c04cad0
Pat <patrick.fleckenstein@outlook.com>
2022-06-13 03:42:55 -0700

    added models for PVS app

--- PressureVesselStressApp/models.py
+++ PressureVesselStressApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class PressureVesselStressApp(models.Model)
+    SURFACES = ('Inner', 'Outer')
+    CAPPED = ('Capped', 'Uncapped')
+
+    surface = models.CharField(max_length=1, choices=SURFACES)
+    capped = models.CharField(max_length=1, choices=CAPPED)
+    pouter = models.FloatField(max_length=100)
+    pinner = models.FloatField(max_length=100)
+    douter = models.FloatField(max_length=100)
+    dinner = models.FloatField(max_length=100)
