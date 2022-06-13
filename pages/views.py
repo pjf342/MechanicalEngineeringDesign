87a989ea735f7171c6c829058d67ebf0a1451d40
Pat <patrick.fleckenstein@outlook.com>
2022-06-13 05:31:09 -0700

    added html templates with basic headers

--- pages/views.py
+++ pages/views.py
@@ -12,16 +12,19 @@ def home_view(request, *args, **kwargs):
 def pvs_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request.user)
-    return HttpResponse('<h1> Pressure Vessel Stresses </h1>')
+    # return HttpResponse('<h1> Pressure Vessel Stresses </h1>')
+    return render(request, 'pvs.html', {})
 
 
 def princ_str_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request.user)
-    return HttpResponse('<h1> Principal Stresses </h1>')
+    # return HttpResponse('<h1> Principal Stresses </h1>')
+    return render(request, 'principal.html', {})
 
 
 def cw_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request.user)
-    return HttpResponse('<h1> Strength and Cold Working </h1>')
+    # return HttpResponse('<h1> Strength and Cold Working </h1>')
+    return render(request, 'cw.html', {})
