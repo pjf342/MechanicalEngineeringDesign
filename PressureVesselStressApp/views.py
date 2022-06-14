d340138b45155c2069198811777bb720095fd66c
Pat <patrick.fleckenstein@outlook.com>
2022-06-14 12:08:56 -0700

    registered model to admin

--- PressureVesselStressApp/views.py
+++ PressureVesselStressApp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .forms import PressureVesselStressForm
 # Create your views here.
+
+def pressure_vessel_create_view(request):
+    form = PressureVesselStressForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        context = {
+            'form': form
+        }
+        return render(request, "")
+
+def pressure_vessel_detail_view(request):
+    obj = Pres
+    return render(request, "Pressure")
