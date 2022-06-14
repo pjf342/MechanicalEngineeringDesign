65cad2f023b07873b810ca7fa008d1bbd4f12edb
Pat <106631373+pjf342@users.noreply.github.com>
2022-06-14 02:04:30 -0700

    remove secret key

--- MEDdjango/settings.py
+++ MEDdjango/settings.py
@@ -23,7 +23,7 @@ print(TEMPLATE_DIR)
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uh6m)u$_kmxgzjz9ppw&hbw#4q=80za!w#xdd$+%1va&woxa_a'
+SECRET_KEY = secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
