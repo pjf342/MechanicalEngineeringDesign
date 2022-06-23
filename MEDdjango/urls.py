73544d7a9699f40a1856435c08bd637ec690e18b
Pat <patrick.fleckenstein@outlook.com>
2022-06-23 15:19:17 -0700

    added word 'cylindrical' to pressure vessel stress

--- MEDdjango/urls.py
+++ MEDdjango/urls.py
@@ -5,5 +5,5 @@ from pages.views import home_view, pvs_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
-    path('pressure_vessel_stress/', pvs_view, name='pv'),
+    path('cylindrical_pressure_vessel_stress/', pvs_view, name='pv'),
 ]
