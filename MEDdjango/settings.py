e0f9009b5d3d0da69b98232b32d9f78421baa3f1
Pat <patrick.fleckenstein@outlook.com>
2022-06-23 15:17:13 -0700

    added localip to allowed hosts

--- MEDdjango/settings.py
+++ MEDdjango/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-uh6m)u$_kmxgzjz9ppw&hbw#4q=80za!w#xdd$+%1va&woxa_a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.104']
 
 
 # Application definition
